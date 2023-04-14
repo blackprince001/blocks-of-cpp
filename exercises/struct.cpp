d008d50461c4d59397e368c4919708a79d673138
blackprince001 <appiahboaduprince@gmail.com>
2023-04-14 08:29:30 +0000

    no sig

--- exercises/struct.cpp
+++ exercises/struct.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
 
 struct Exercise {
-  char unit_0;
-  bool unit_bool;
-  int unit_1;
-  double unit_2;
-  long double unit_3;
+    char unit_0;
+    bool unit_bool;
+    int unit_1;
+    double unit_2;
+    long double unit_3;
 
-  void print() {
-    std::cout << unit_0 << unit_1 << unit_2 << unit_3 << unit_bool;
-  }
+    void print()
+    {
+        std::cout << unit_0 << unit_1 << unit_2 << unit_3 << unit_bool;
+    }
 };
 
+int main()
+{
+    Exercise n_obj;
+    std::cout << "Structs have been created successfully";
+}

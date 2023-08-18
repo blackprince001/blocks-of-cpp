4d091ef90ae3e6d7f4d85dc169330347fd3eb792
blackprince001 <appiahboaduprince@gmail.com>
2023-08-18 10:03:31 +0000

    reformat file

--- concepts/named_parameters.cpp
+++ concepts/named_parameters.cpp
@@ -5,24 +5,20 @@ struct Person {
     unsigned int age;
 };
 
-#define get_person_age(...) _get_person_age((struct Person) { __VA_ARGS__ })
-int _get_person_age(struct Person new_person)
-{
-    return new_person.age;
-}
+#define get_person_age(...) _get_person_age((struct Person){__VA_ARGS__})
+int _get_person_age(struct Person new_person) { return new_person.age; }
 
-#define get_person_name(...) _get_person_name((struct Person) { __VA_ARGS__ })
-const char* _get_person_name(struct Person new_person)
-{
+#define get_person_name(...) _get_person_name((struct Person){__VA_ARGS__})
+const char* _get_person_name(struct Person new_person) {
     return new_person.name;
 }
 
-int main(void)
-{
+int main(void) {
     int person_age = get_person_age(.name = "Prince", .age = 18, );
     const char* person_name = get_person_name(.name = "Kwabena", .age = 23, );
 
     std::cout << person_name << person_age;
+    std::cout << person_age * 12;
 
     return 0;
 }
